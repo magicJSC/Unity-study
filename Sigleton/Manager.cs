bba2f6169bf6a5c3ce97eb97e300cab0fe3e5f6c
umghiking <jeonghaejun004@gmail.com>
2023-06-19 08:29:31 +0900

    Upload

---
+++ Sigleton/Manager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    static Manager s_instance;
+    static public Manager Instance { get { return s_instance; } }
+    void Start()
+    {
+        Init();
+    }
+
+    static void Init()
+    {
+        if (s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Manager");
+            if (go == null)
+            {
+                go = new GameObject { name = "@Manager" };
+                go.AddComponent<Manager>();
+            }
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Manager>();
+        }
+
+    }
+}
