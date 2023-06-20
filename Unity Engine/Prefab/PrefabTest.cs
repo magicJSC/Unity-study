9f0a6e9d3d831e2dd82f75127beabae6a2eb5ab3
umghiking <jeonghaejun004@gmail.com>
2023-06-20 23:49:01 +0900

    Upload

---
+++ Unity Engine/Prefab/PrefabTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabTest : MonoBehaviour
+{
+    [SerializeField]
+    GameObject prefab;
+    GameObject tank;
+    void Start()
+    {
+        tank = Manager.Resource.Instantiate("Tank");
+        prefab = Resources.Load<GameObject>("Prefab/Tank");  //[SerializeField]를 쓰지 않을 떄 사용 => resouses폴더에서 Prefab에 있는 Tank를 오브젝트로 설정
+        tank = Instantiate(prefab); //Gameobject인 prefab을 생성
+        Destroy(tank, 3.0f);  //3초뒤에 삭제
+    }
+
+
+
+}
