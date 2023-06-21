954d3f721243172413b8b5406b6708ebc7883cab
umghiking <jeonghaejun004@gmail.com>
2023-06-21 14:03:48 +0900

    Upload

---
+++ Unity Engine/Raycast.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCollider : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        Vector3 look = transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);  //Raycast의 범위에 가장 앞에 있는 물체를 확인함
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, look, out hit, 10))
+        {
+            Debug.Log($"{hit.collider.gameObject.name}");
+        }
+
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position + Vector3.up, look * 10);   //Raycast의 범위에 모든 오브젝트를 확인함
+        foreach (RaycastHit hit in hits)
+        {
+            Debug.Log($"{hit.collider.gameObject.name}");
+        }
+    }
+}
---
+++ Unity Engine/collider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCollider : MonoBehaviour
+{
+    /*발동 조건
+    1. 나 또는 부딪힌 오브젝트에 Rigidbody가 있어야한다 (isKinematic : Off)
+    2. 나에게 Collider가 있어야한다 (IsTrigger : Off)
+    3. 부딪친 오브젝트에 Collider가 있어야한다 (IsTrigger : Off)
+    */
+    private void OnCollisionEnter(Collision collision)  //collision은 부딪친 오브젝트
+    {
+        Debug.Log($"collision{collision.gameObject.name}");
+    }
+
+    /*
+    1. 둘다 collider가 있어야한다
+    2. 둘 중 하나는 istrigger가 되야한다
+    3. 둘 중 하나는 RigidBody가 있어야한다
+    */
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log($"Trigger{other.gameObject.name}");
+    }
