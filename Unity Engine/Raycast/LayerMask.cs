4328fede779096eb5dda90694332847a16563216
umghiking <jeonghaejun004@gmail.com>
2023-06-23 23:27:03 +0900

    Upload

--- Unity Engine/Raycast/LayerMask.cs
+++ Unity Engine/Raycast/LayerMask.cs
@@ -1,32 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using Csharp;
 
-public class Raycast2 : MonoBehaviour
+namespace Csharp
 {
-
-    void Start()
+    class Knight
     {
-
-    }
-
-    void Update()
-    {
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
-
-            LayerMask mask = LayerMask.GetMask("Monster") | LayerMask.GetMask("Wall");  //오브젝트의 tag를 얻는다
-            //int mask = (1<<6) | (1<<7);
-
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, 100.0f, mask))
-            {
-                Debug.Log($"{hit.collider.gameObject.tag}");
-            }
+        public int hp;
+        public int at;
+        Knight
+       {
+          
         }
     }
+    class Program
+    {
+        Knight knight = new Knight();   //new Knight를 함과 동시에 값을 전달한다
+    }
 }
+
--- Unity Engine/Text_UI.cs
+++ Unity Engine/Text_UI.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System;    //Type을 사용하기 위해 만든다
 public class Text_UI : MonoBehaviour
 {
     [SerializeField]
     TextMeshProUGUI _text;
     int _score =0;
-    
-    
+
+    Dictionary<Type, UnityEngine.Object[]>_objects=new Dictionary<Type, UnityEngine.Object[]>();
+    enum Texts
+    {
+        PointText,
+        ScoreText
+    }
+    enum Buttons
+    {
+        PointButton
+    }
+
+    private void Start()
+    {
+        Bind<Button>(typeof(Buttons));
+        Bind<Text>(typeof(Texts));
+    }
+    void Bind<T>(Type type)
+    {
+        string[] names = Enum.GetNames(type);
+        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
+        _objects.Add(typeof(T),objects);
+        for(int i=0;i<names.Length;i++)
+        {
+            objects[i] = null;
+        }
+    }
     public void OnButtonClicked()
     {
         _score++;
