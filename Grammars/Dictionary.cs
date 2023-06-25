4b3cc6781b85bc1dc0dbb1070b1306857c7b40b3
umghiking <jeonghaejun004@gmail.com>
2023-06-25 19:20:10 +0900

    Upload

---
+++ Grammars/Dictionary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;  
+
+namespace grammars
+{
+    class Program
+    {
+        class Monster
+        {
+            public int id;
+            public Monster(int id) { this.id = id; }
+        }
+        static void Main(string[] args)
+        {
+            //int를 key라하고 Monster를 value라 부르는데 key가 있으면 value를 쉽게 찾을수있다
+            Dictionary<int,Monster> dic = new Dictionary<int,Monster>();
+            
+            //사용 방법
+            dic.Add(1, new Monster(1));
+            
+
+            Monster mon;
+            bool found = dic.TryGetValue(1,out mon);  //0에 값을 찾아소 mon에 값을 넣는다 성공하면 true
+            dic.Remove(1);
+            dic.Clear();
+        }
+    }
+}
+
---
+++ Grammars/List.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;  
+
+namespace grammars
+{
+    class Program
+    {
+        class Monster
+        {
+            public int id;
+            public Monster(int id) { this.id = id; }
+        }
+        static void Main(string[] args)
+        {
+            //int를 key라하고 Monster를 value라 부르는데 key가 있으면 value를 쉽게 찾을수있다
+            List<int> list = new List<int>();
+            
+            //사용 방법
+            list.Add(1);  //리스트에 1을 넣는다
+
+
+
+            bool found = list.Remove(1); //지운다면 true
+
+        }
+    }
+}
+
