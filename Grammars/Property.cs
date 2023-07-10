1936e9c24e9d34edf014fe6c42b9aec5dbcf5920
umghiking <jeonghaejun004@gmail.com>
2023-07-10 16:53:19 +0900

    Upload

---
+++ Grammars/Property.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CShep
+{
+    class Program
+    {
+        class Knight
+        {
+            protected hp;
+            public int HP;
+            {
+                get { return hp; } set {  this.hp = value; }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Knight knight = new Knight();
+            
+            knight.HP = 10;  //값을 넣어줄떄
+            int hp = knight.HP;   //닶을 가져올떄
+        }
+    }
+}
