250884c1b5d1cc9eac55be394f2a29de39c19e23
umghiking <jeonghaejun004@gmail.com>
2023-07-19 08:34:43 +0900

    Upload

---
+++ Unity Manager/UI/UI_Event.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+public class UI_Event : MonoBehaviour, IPointerClickHandler,IDragHandler
+{
+    public Action<PointerEventData> OnClickHandler = null;              //Action을 해준다
+    public Action<PointerEventData> OnDragHandler = null;
+
+
+    public void OnDrag(PointerEventData eventData)                      //interface
+    {
+        if (OnDragHandler == null)
+        {
+            OnDragHandler.Invoke(eventData);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (OnClickHandler == null)
+        {
+            OnClickHandler.Invoke(eventData);
+        }
+    }
+}
