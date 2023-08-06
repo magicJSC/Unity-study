d125ab1c4c736c3dbe79c8ab2614101040ccbb15
umghiking <jeonghaejun004@gmail.com>
2023-08-06 23:15:05 +0900

    Upload

---
+++ MyHoneyTips/Region.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    internal class Rigion
+    {
+        //이걸로 여기 있는 내용을 요약할 수 있다
+        #region 이름
+        int hp;
+        float mp;
+        void Attack()
+        {
+            hp++;
+        }
+        #endregion
+    }
+}
