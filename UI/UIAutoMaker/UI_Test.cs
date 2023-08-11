6164f44878a9bf58c5b0b579b3376a6bbb848198
umghiking <jeonghaejun004@gmail.com>
2023-08-11 20:23:37 +0900

    Upload

---
+++ UI/UIAutoMaker/UI_Test.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UI_Test : UI_Popup
+{
+    Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, UnityEngine.Object[]>();
+
+   enum Buttons
+    {
+        PointButton
+    }
+    enum Texts
+    {
+        PointText,
+        ScoreText
+    }
+    enum GameObjects
+    {
+        TestObject
+    }
+    enum Images
+    { 
+       ItemIcon
+    }
+
+    private void Start()
+    {
+       Init();
+    }
+
+    public override void Init()
+    {
+        base.Init();
+        Bind<Button>(typeof(Buttons));
+        Bind<TextMeshProUGUI>(typeof(Texts));
+        Bind<GameObject>(typeof(GameObjects));
+        Bind<Image>(typeof(Images));
+
+
+        GetButton((int)Buttons.PointButton).gameObject.AddUIEvent(OnButtonClicked); //Extention스크립트가 있어야 가능함
+        GameObject go = GetImage((int)Images.ItemIcon).gameObject;
+
+        AddUIEvent(go, (PointerEventData data) => { go.transform.position = data.position; }, Define.UIEvent.Drag);
+    }
+    int _score = 0;
+    public void OnButtonClicked(PointerEventData data)
+    {
+        _score++;
+        GetText((int)Texts.ScoreText).text = $"{_score}점";
+    }
+}
